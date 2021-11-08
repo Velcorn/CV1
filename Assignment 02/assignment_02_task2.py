46b0416671dda1b90b5c97fd359dd1490eb97207
Velcorn <Velcorn@users.noreply.github.com>
2021-11-08 13:41:43 +0100

    Begin assignment_02_task3.py

--- Assignment 02/assignment_02_task2.py
+++ Assignment 02/assignment_02_task2.py
@@ -3,10 +3,8 @@ Chams Alassil Khoury, 7161852
 Adrian Westphal, 6940017
 Jan Willruth, 6768273
 """
-import sys
 
 import matplotlib.pyplot as plt
-import numpy as np
 from skimage.io import imread
 from skimage.util import random_noise
 from skimage.filters import gaussian, sobel
--- Assignment 02/assignment_02_task3.py
+++ Assignment 02/assignment_02_task3.py
@@ -4,4 +4,13 @@ Adrian Westphal, 6940017
 Jan Willruth, 6768273
 """
 
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.io import imread
+from skimage.color import rgb2gray
+from skimage.transform.pyramids import pyramid_gaussian
 
+img = imread("visual_attention.png")
+img_gray = rgb2gray(img)
+
+# Pyramid stuff

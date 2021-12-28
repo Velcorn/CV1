a68b4af9f8cc25022fe7af12c4d3232255252def
Velcorn <Velcorn@users.noreply.github.com>
2021-12-28 22:51:31 +0100

    Minor text fixes

--- Assignment 04/assignment_04_task3.py
+++ Assignment 04/assignment_04_task3.py
@@ -42,13 +42,21 @@ for gts in gt_segments:
         imgs_coords = list(zip(*imgs_coords))
         # If any overlap exists, add to segmentation area
         if any(coord in gt_coords for coord in imgs_coords):
-            seg_area += np.count_nonzero(img_seg == imgs)
+            seg_area += len(imgs_coords)
     # Calculate error for ground truth segment and increment total error
     error = (seg_area - gt_area) / gt_area
     total_error += error
 
-    # Print error per segment
+    # Print segment error
     print(f"Error for segment {gts:02n}: {round(error, 2)}")
 
-# Calculate and print average error
+# Calculate and print average error over ground truth segments
 print(f"Average segmentation error: {round(total_error / len(gt_segments), 2)}")
+
+
+"""
+Q: How does the average undersegmentation error change when you increase the desired
+number of superpixels n? Why?
+A: It gets lower because there are more segments/labels than there are in the ground truth, resulting in less area 
+for the sum which in turn results in smaller (more likely to be negative) numerators and thus smaller error(s).
+"""
